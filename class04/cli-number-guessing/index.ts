832ad4be4b72e396fcecb1f9cfff6a83abf5eba0
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-23 17:42:48 +0500

    Create random number guessing game

---
+++ class04/cli-number-guessing/index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.round(Math.random() * 5);
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess"
+    }
+]);
+
+console.log(`Random Number: ${randomNumber}`);
+
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations! You guessed the right number.");
+} else {
+    console.log("Sorry! You guessed wrong number;");
+}
